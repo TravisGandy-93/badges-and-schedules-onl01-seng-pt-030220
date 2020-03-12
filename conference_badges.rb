b038f602b9f7197be1b1aa93ccda52e9a3e3db75
Travis Gandy <travis_gandy@yahoo.com>
2020-03-12 14:59:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,7 @@ end
 def batch_badge_creator(speakers)
   speakers = [ "Edsger" , "Ada" , "Charles" , "Alan" , "Grace" , "Linus" , "Matz"]
   speakers.each do |speaker|
-     "Hello, my name is #{speaker}." 
+     speakerBadge = "Hello, my name is #{speaker}." 
   end
+  speakerBadge
 end
